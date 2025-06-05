c54481682d2a681a9ed4d54feff92b4bf0f889af
Tan Yawen <2749934612@qq.com>
2025-06-05 10:12:12 +0800

    初始化

---
+++ src/test/java/com/example/strategydesignpatterndemo/StrategyDesignPatternDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.strategydesignpatterndemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrategyDesignPatternDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
